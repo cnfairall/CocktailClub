b868b20fd88ebec8780b380b075e9fbe73ce90db
Courtney Fairall <101303134+cnfairall@users.noreply.github.com>
2024-06-18 10:57:41 -0500

    Update [id].js

--- pages/savedcocktails/[id].js
+++ pages/savedcocktails/[id].js
@@ -1,11 +1,12 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import SavedCocktail from '../../components/cards/SavedCocktail';
 import { getSavedCocktailDetails } from '../../api/SavedCocktailsApi';
 
 export default function CocktailDetailPage() {
   const router = useRouter();
   const { id } = router.query;
+  const ref = useRef();
   const [savedCocktail, setSavedCocktail] = useState({});
 
   const getCocktail = () => {
@@ -14,7 +15,7 @@ export default function CocktailDetailPage() {
 
   useEffect(() => {
     getCocktail();
-  }, [id]);
+  }, [ref.current, id]);
 
   return (
     <div className="center">
