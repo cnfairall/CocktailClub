6689af91353adbc267f97b50fdc0d9cd305e92ad
Courtney Fairall <101303134+cnfairall@users.noreply.github.com>
2024-07-01 10:05:48 -0500

    Update UsersApi.js

--- api/UsersApi.js
+++ api/UsersApi.js
@@ -1,6 +1,6 @@
-import { clientCredentials } from '../utils/client';
+// import { clientCredentials } from '../utils/client';
 
-const endpoint = clientCredentials.databaseURL;
+const endpoint = "cocktailclub.app";
 
 const getSingleUser = (id) => new Promise((resolve, reject) => {
   fetch(`${endpoint}/api/users/${id}`, {
