a49daecc06cd128021cf223d34346cc646877c6c
Courtney Fairall <101303134+cnfairall@users.noreply.github.com>
2024-07-01 10:04:42 -0500

    Update GlassesApi.js

--- api/GlassesApi.js
+++ api/GlassesApi.js
@@ -1,6 +1,6 @@
-import { clientCredentials } from '../utils/client';
+// import { clientCredentials } from '../utils/client';
 
-const endpoint = clientCredentials.databaseURL;
+const endpoint = "cocktailclub.app";
 
 const getGlasses = () => new Promise((resolve, reject) => {
   fetch(`${endpoint}/api/glasses`, {
