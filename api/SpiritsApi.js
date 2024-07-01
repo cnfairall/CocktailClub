1b0d36587aeebc0a4538e5a7d21132608527d34d
Courtney Fairall <101303134+cnfairall@users.noreply.github.com>
2024-07-01 10:05:27 -0500

    Update SpiritsApi.js

--- api/SpiritsApi.js
+++ api/SpiritsApi.js
@@ -1,6 +1,6 @@
-import { clientCredentials } from '../utils/client';
+// import { clientCredentials } from '../utils/client';
 
-const endpoint = clientCredentials.databaseURL;
+const endpoint = "cocktailclub.app";
 
 const getSpirits = () => new Promise((resolve, reject) => {
   fetch(`${endpoint}/api/spirits`, {
